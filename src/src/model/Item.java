cb0af38efaa094d860c60364183957a2a988f1de
dojo2023-inoue <INOUEKOUSEIplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-15 16:42:05 +0900

    Update Item.java

--- src/src/model/Item.java
+++ src/src/model/Item.java
@@ -2,4 +2,61 @@ package model;
 
 public class Item {
 
+	private int item_id;
+	private int user_id;
+	private String daily_name;
+	private String daily_unit;
+	private String item_name;
+	private String item_price;
+	private String item_volume;
+	private String item_memo;
+	public int getItem_id() {
+		return item_id;
+	}
+	public void setItem_id(int item_id) {
+		this.item_id = item_id;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getDaily_name() {
+		return daily_name;
+	}
+	public void setDaily_name(String daily_name) {
+		this.daily_name = daily_name;
+	}
+	public String getDaily_unit() {
+		return daily_unit;
+	}
+	public void setDaily_unit(String daily_unit) {
+		this.daily_unit = daily_unit;
+	}
+	public String getItem_name() {
+		return item_name;
+	}
+	public void setItem_name(String item_name) {
+		this.item_name = item_name;
+	}
+	public String getItem_price() {
+		return item_price;
+	}
+	public void setItem_price(String item_price) {
+		this.item_price = item_price;
+	}
+	public String getItem_volume() {
+		return item_volume;
+	}
+	public void setItem_volume(String item_volume) {
+		this.item_volume = item_volume;
+	}
+	public String getItem_memo() {
+		return item_memo;
+	}
+	public void setItem_memo(String item_memo) {
+		this.item_memo = item_memo;
+	}
+
 }
