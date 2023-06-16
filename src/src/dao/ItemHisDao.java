da7e08e1f3d6de5d6eaa0d3166838d58e8128c3b
dojo2023-inoue <INOUEKOUSEIplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-16 17:32:09 +0900

    Update ItemHisDao.java

--- src/src/dao/ItemHisDao.java
+++ src/src/dao/ItemHisDao.java
@@ -73,58 +73,5 @@ public class ItemHisDao {
 		return itemlist;
 	}
 
-	// 引数cardで指定されたレコードを登録し、成功したらtrueを返す
-	public boolean insert(Item item) {
-		Connection conn = null;
-		boolean result = false;
 
-		try {
-			// JDBCドライバを読み込む
-			Class.forName("org.h2.Driver");
-
-			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/myBC", "sa", "");
-
-			// SQL文を準備する
-			String sql = "insert into BC (NAME, COMPANY, DEPART, POSITION, ADDRESS, TEL, MAIL, ANIMAL) values (?, ?, ?, ?, ?, ?, ?, ?)";
-			PreparedStatement pStmt = conn.prepareStatement(sql);
-
-			// SQL文を完成させる
-			pStmt.setString(1, card.getName());
-			pStmt.setString(2, card.getCompany());
-			pStmt.setString(3, card.getDepart());
-			pStmt.setString(4, card.getPosition());
-			pStmt.setString(5, card.getAddress());
-			pStmt.setString(6, card.getTel());
-			pStmt.setString(7, card.getMail());
-			pStmt.setString(8, card.getAnimal());
-
-
-
-			// SQL文を実行する
-			if (pStmt.executeUpdate() == 1) {
-				result = true;
-			}
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		finally {
-			// データベースを切断
-			if (conn != null) {
-				try {
-					conn.close();
-				}
-				catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-		// 結果を返す
-		return result;
-	}
 }
