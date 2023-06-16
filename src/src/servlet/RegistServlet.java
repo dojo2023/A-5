83ddabcadab549d8488fe30f28dd01ce64d05cab
dojo2023-inoue <INOUEKOUSEIplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-16 17:32:15 +0900

    Update RegistServlet.java

--- src/src/servlet/RegistServlet.java
+++ src/src/servlet/RegistServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,7 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.ItemDao;
 import dao.ItemHisDao;
+import model.Item;
 
 /**
  * Servlet implementation class RegistServlet
@@ -21,10 +24,10 @@ public class RegistServlet extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public RegistServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/* public RegistServlet() {
+	    super();
+	    // TODO Auto-generated constructor stub
+	}*/
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -32,10 +35,11 @@ public class RegistServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 
-		ItemHisDao IHDAO = new ItemHisDao();
-		String due = IHDAO.select();
+		ItemHisDao IHDao = new ItemHisDao();
+		List<Item> itemlist = IHDao.select();
+		System.out.println(itemlist.size()+"bbbbbb");
 		//一覧検索
-		request.setAttribute("due", due);
+		request.setAttribute("itemlist", itemlist);
 
 
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist.jsp");
@@ -47,7 +51,37 @@ public class RegistServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//ユーザーIDは常に必要
+		//HttpSession session = request.getSession();
+		//session.getAttribute('userId');
+
+
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		int userId = 1;
+		String dailyName = request.getParameter("dailyName");
+		String dailySelect = request.getParameter("dailySelect");
+		String itemName = request.getParameter("itemName");
+		int itemPrice = Integer.parseInt(request.getParameter("itemPrice"));
+		int itemVolume = Integer.parseInt(request.getParameter("itemVolume"));
+		String dailyUnit = request.getParameter("dailyUnit");
+		String itemMemo = request.getParameter("itemMemo");
+
+		if (!(dailySelect.equals("create"))) {
+			dailyName = dailySelect;
+		}
+
+		// 登録処理を行う
+		ItemDao IDao = new ItemDao();
+		boolean result = false;
+		if (IDao.insert(new Item(userId, dailyName, dailyUnit, itemName, itemPrice, itemVolume, itemMemo))) {	// 登録成功
+			result = true;
+		}
+		request.setAttribute("result",result);
+
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
