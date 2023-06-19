8fbc8e5dd2c55825d3acb88661f432d40c1a150d
dojo2023-akasabi <130334422+dojo2023-akasabi@users.noreply.github.com>
2023-06-19 19:47:53 +0900

    Update ListServlet.java

--- src/src/servlet/ListServlet.java
+++ src/src/servlet/ListServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.HWHisDao;
+import model.HW;
 
 /**
  * Servlet implementation class ListServlet
@@ -13,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ListServlet")
 public class ListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,16 +32,28 @@ public class ListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		request.setCharacterEncoding("UTF-8");
 
+		HWHisDao hwHis = new HWHisDao();
+		List<HW> hwList = hwHis.select();
+
+
+		// 検索結果をリクエストスコープに格納する
+		request.setAttribute("hwList", hwList);
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/list.jsp");
+		dispatcher.forward(request, response);
+
+
+		/*
+				try {
+					SimpleDateFormat sdf = new SimpleDateFormat("yyyy--MM-dd");
+		    Date date = sdf.parse(item_start);
+		    System.out.println(date);
+				} catch (java.text.ParseException e) {
+					e.printStackTrace();
+				}*/
+
+	}
 }
