4110c197d1bada120bca14f4540d2af61a0c8ce7
dojo2023-sasaki-s <130334868+dojo2023-sasaki-s@users.noreply.github.com>
2023-06-19 17:27:53 +0900

    Update LoginServlet.java

--- src/src/servlet/LoginServlet.java
+++ src/src/servlet/LoginServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.UsersDao;
+import model.User;
 
 /**
  * Servlet implementation class LoginServlet
@@ -15,14 +19,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -37,9 +33,27 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// リクエストパラメータを取得する
+				request.setCharacterEncoding("UTF-8");
+				String user_name = request.getParameter("user_name");
+				String user_pass = request.getParameter("user_pass");
+
+
+		UsersDao UDao = new UsersDao(); //呼び出し
+		User Login = UDao.isLoginOK(user_name, user_pass);
+
+		//ユーザーIDが取得できていたらログイン成功
+		if(Login.getUser_id() != null){
+			HttpSession session = request.getSession();
+			session.setAttribute("Login", Login);//枝豆の房をセッションスコープに保存
+			// ホームサーブレットにリダイレクトする
+			//今だけ　新規登録画面に飛ぶ
+			response.sendRedirect("AccountServlet");
+  		}
+		// ログイン失敗
+		else {
+			response.sendRedirect("LoginServlet");
+		}
+
 	}
-
 }
-
