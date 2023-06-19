141a0e0e8c88a8d5eb0dea55cddcbefd41720afc
dojo2023-kojima <KOJIMAMIZUKIplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-19 15:48:39 +0900

    Update ItemEditServlet.java

--- src/src/servlet/ItemEditServlet.java
+++ src/src/servlet/ItemEditServlet.java
@@ -1,11 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+import java.net.http.HttpResponse;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.HWDao;
+import dao.ItemDao;
 
 /**
  * Servlet implementation class ItemEditServlet
@@ -13,29 +18,38 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ItemEditServlet")
 public class ItemEditServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ItemEditServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	 /**
+     * @see HttpServlet#doget(httpSelveletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpResponse response) throws ServletException, IOException{
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null){
+			response.sendRedirect("/dojo6/LoginServlet");
+			return;
+		}
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/ItemEdit.jsp");
+		dispatcher.forward(request, response);
 	}
+
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	    HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null){
+			response.sendRedirect("/dojo6/LoginServlet");
+			return;
+		}
+
+		request.setCharacterEncoding("UTF-8");
+		request.
+	
+
+
 	}
 
 }
