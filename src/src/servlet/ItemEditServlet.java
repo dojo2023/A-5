58a50cd799c7e6b24323fd29dad43087c63249b8
dojo2023-kojima <KOJIMAMIZUKIplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-20 10:01:37 +0900

    Update ItemEditServlet.java

--- src/src/servlet/ItemEditServlet.java
+++ src/src/servlet/ItemEditServlet.java
@@ -2,7 +2,7 @@ package servlet;
 
 import java.io.IOException;
 import java.net.http.HttpResponse;
-
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import dao.HWDao;
 import dao.ItemDao;
+import dao.ItemHisDao;
+import model.HW;
+import model.Item;
+
 
 /**
  * Servlet implementation class ItemEditServlet
@@ -24,17 +28,20 @@ public class ItemEditServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpResponse response) throws ServletException, IOException{
 		HttpSession session = request.getSession();
-		if (session.getAttribute("id") == null){
-			response.sendRedirect("/dojo6/LoginServlet");
-			return;
-		}
+			//if (session.getAttribute("id") == null){
+				//response.sendRedirect("/dojo6/LoginServlet");
+				//ret
+        ItemHisDao IHDao = new ItemHisDao();
+ 		//items、item_historyの中のデータを全部持ってくる
+		List<Item> itemlist = IHDao.select();
+		//foreachをコンソールに出して中身を確認
+		itemlist.forEach(event -> System.out.println(event.getDailyName()));
+		request.setAttribute("itemlist", itemlist);
 
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/ItemEdit.jsp");
 		dispatcher.forward(request, response);
 	}
-
 	
-
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -46,9 +53,18 @@ public class ItemEditServlet extends HttpServlet {
 		}
 
 		request.setCharacterEncoding("UTF-8");
-		request.
+		String itemName = request.getParameter("itemName");
+		String itemCapacity = request.getParameter("itemCapacity");
+		String itemPrice = request.getParameter("itemPrice");
+		String startDate = request.getParameter("startDate");
+		String endDate = request.getParameter("endDate");
+		String Itemremarks = request.getParameter("Itemremarks");
+
+
+
 	
 
+        //フォワード処理
 
 	}
 
