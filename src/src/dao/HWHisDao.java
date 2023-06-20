c9af8d56b55eca518f539580e98e20e2f47fb6b6
dojo2023-akasabi <130334422+dojo2023-akasabi@users.noreply.github.com>
2023-06-20 18:10:23 +0900

    Update HWHisDao.java

--- src/src/dao/HWHisDao.java
+++ src/src/dao/HWHisDao.java
@@ -16,38 +16,38 @@ public class HWHisDao {
         List<HW> hwList = new ArrayList<HW>();
 
         try {
-            // JDBCドライバを読み込む
-            Class.forName("org.h2.Driver");
+			 // JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
 
-            // データベースに接続する
-            conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/Yakou", "sa", "");
-            String sql = "SELECT hw.hw_id, hw.user_id, hw.hw_name, hw.hw_memo, his.hw_his_id, his.hw_date, his.hw_due, his.hw_flag FROM hw_history AS his JOIN houseworks AS hw ON his.hw_id = hw.hw_id";
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/Yakou", "sa", "");
+			String sql = "SELECT hw.hw_id, hw.user_id,hw.hw_freq, hw.hw_name, hw.hw_memo, his.hw_his_id, his.hw_date, his.hw_due, his.hw_flag FROM hw_history AS his JOIN houseworks AS hw ON his.hw_id = hw.hw_id";
 
-            PreparedStatement pStmt = conn.prepareStatement(sql);
 
-            // SQL文を実行し、結果表を取得する
-            ResultSet rs = pStmt.executeQuery();
+        	PreparedStatement pStmt = conn.prepareStatement(sql);
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
 
             // 結果表をコレクションにコピーする
-            while (rs.next()) {
-                System.out.println(rs.getString("HW_NAME") + "完了");
-                HW hw = new HW();
-                hw.setHwId(rs.getInt("HW_ID"));
-                hw.setUserId(rs.getInt("USER_ID"));
-                hw.setHwName(rs.getString("HW_NAME"));
-                hw.setHwFreq(rs.getInt("HW_FREQ"));
-                hw.setHwMemo(rs.getString("HW_MEMO"));
-                hw.setHwHisId(rs.getInt("HW_HIS_ID"));
-                hw.setHwDue(rs.getDate("HW_DUE"));
-                hw.setHwDate(rs.getDate("HW_DATE"));
-                hw.setHwFlag(rs.getBoolean("HW_FLAG"));
-                hwList.add(hw);
-            }
+			 while (rs.next()) {
+			    System.out.println(rs.getString("HW_NAME") + "完了");
+			    HW hw = new HW();
+			    hw.setHwId(rs.getInt("HW_ID"));
+			    hw.setUserId(rs.getInt("USER_ID"));
+			    hw.setHwName(rs.getString("HW_NAME"));
+			    hw.setHwFreq(rs.getInt("HW_FREQ"));
+			    hw.setHwMemo(rs.getString("HW_MEMO"));
+			    hw.setHwHisId(rs.getInt("HW_HIS_ID"));
+			    hw.setHwDue(rs.getDate("HW_DUE"));
+			    hw.setHwDate(rs.getDate("HW_DATE"));
+			    hw.setHwFlag(rs.getBoolean("HW_FLAG"));
+			    hwList.add(hw);
+			}
         } catch (SQLException e) {
-            e.printStackTrace(); // エラーハンドリング修正
+            e.printStackTrace();
             hwList = null;
         } catch (ClassNotFoundException e) {
-            e.printStackTrace(); // エラーハンドリング修正
+            e.printStackTrace();
             hwList = null;
         } finally {
             // データベースを切断
@@ -55,7 +55,7 @@ public class HWHisDao {
                 try {
                     conn.close();
                 } catch (SQLException e) {
-                    e.printStackTrace(); // エラーハンドリング修正
+                    e.printStackTrace();
                     hwList = null;
                 }
             }
