8f439042f62a55fc5a5d955a53a910b7ad49bd74
dojo2023-inoue <INOUEKOUSEIplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-21 14:55:02 +0900

    Update ItemDao.java

--- src/src/dao/ItemDao.java
+++ src/src/dao/ItemDao.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import model.Item;
@@ -61,5 +62,50 @@ public class ItemDao {
 
 			// 結果を返す
 			return result;
+		}
+
+		public int getMaxItemId() {
+			Connection conn = null;
+			int maxItemId = 0;
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/Yakou", "sa", "");
+				String sql = "SELECT MAX(item_id) AS maxId FROM items WHERE user_id = ? ";
+				PreparedStatement  pStmt = conn.prepareStatement(sql);
+				//user_idの？に1を入れる
+				pStmt.setString(1, "1");
+
+
+				// SQL文を実行し、結果表を取得する
+				ResultSet rs = pStmt.executeQuery();
+
+				// 結果を取得
+				rs.next();
+				maxItemId = rs.getInt("maxId");
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			// 結果を返す
+			return maxItemId;
 		}
 }
