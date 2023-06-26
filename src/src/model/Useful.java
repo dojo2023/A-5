027ca4b12240598da44dd10b25a1166e7903bba0
dojo2023-sawamoto <130334553+dojo2023-sawamoto@users.noreply.github.com>
2023-06-26 18:09:41 +0900

    Update Useful.java

--- src/src/model/Useful.java
+++ src/src/model/Useful.java
@@ -36,4 +36,34 @@ public class Useful {
 		Date itemDue = calendar.getTime();
 		return itemDue;
 	}
+
+	public Date getDate(){
+		Date now = new Date();
+        // Calendarオブジェクトを作成し、日時を設定
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(now);
+        // 時間を0時0分0秒に設定
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        // 設定した日時を取得
+        Date newDate = calendar.getTime();
+		return newDate;
+	}
+	public Date getDatePlus(){
+		Date now = new Date();
+        // Calendarオブジェクトを作成し、日時を設定
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(now);
+        // 時間を0時0分0秒に設定
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        // 設定した日時を取得
+        Date newDate = calendar.getTime();
+		return newDate;
+	}
 }
